8013a4093e4bb7798c634ec962d69076cdbfce46
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2022-04-02 23:20:42 +0100

    adding high level overview to contracts

--- atoms/openlab/index.js
+++ atoms/openlab/index.js
@@ -1,9 +1,11 @@
 import WhatIsOpenlab from './what_is_openlab.md'
+import RunningAService from './running_a_service.md'
 import AddingAService from './adding_a_service.md'
-import GetInvolved from './get_involved.md'
+
 
 export default {
   WhatIsOpenlab,
+  RunningAService,
   AddingAService,
   GetInvolved
 }
